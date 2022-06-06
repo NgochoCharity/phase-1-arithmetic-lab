b4803e57178d022a6f465b1f777711ae144807d5
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-06-06 23:55:20 +0300

    commit tree de5d7be7307ce6b5089e52a870fe412e98357ef5

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+
+//Generates a random number
+const random = Math.floor(Math.random() * 100) ;
+
+// Modulus expression
+const num3 = 24;
+const num4 = 10;
+
+const mod = num3 % num4;
+
+//Max Value
+const max = Math.max(2, 5, 7, 11, 13, 20);
